c98eb0588f7980858397358e185f9db73431dd46
JoosepA <j_andrespuk@hotmail.com>
2020-10-10 15:20:27 +0300

    - about to fork up SpringSecurityConfigurationBasicAuth.java commit just in case shyte hits the fan

--- src/main/java/com/jelly/rest/webservice/restwebservice/basicauth/SpringSecurityConfigurationBasicAuth.java
+++ src/main/java/com/jelly/rest/webservice/restwebservice/basicauth/SpringSecurityConfigurationBasicAuth.java
@@ -1,6 +1,7 @@
 package com.jelly.rest.webservice.restwebservice.basicauth;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,14 +13,13 @@ public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerA
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        ((HttpSecurity)((HttpSecurity)((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)
+        ((HttpSecurity) ((HttpSecurity) ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)
                 http
-                        .authorizeRequests()
+                        .csrf().disable()
+                        .authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest())
-                .authenticated()
-                .and())
-                .formLogin()
-                .and())
-                .httpBasic();
+                        .authenticated().and()))
+                        //.formLogin().and())
+                        .httpBasic();
     }
 }
